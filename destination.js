35a6ab73253eae572cc2fb24b118ed7ec0f95cc2
vuodo <vuodoan@gmail.com>
2022-07-31 10:03:15 +0700

    Update destination.js

--- destination.js
+++ destination.js
@@ -1,8 +1,17 @@
-$(document).ready(function(){
-    $('.container_slider').slick({
-        slidesToShow: 1,
-        infinite: true,
-        arrows: false,
-        dots: true,
-    });
+const $ = document.querySelector.bind(document); // $ will use to exchange document.querySelector('') (faster)
+const $$ = document.querySelectorAll.bind(document); // $$ will use to exchange document.querySelectorAll('') (faster)
+
+const desNavItems = $$('.destination_nav-item');
+const containerDestinations = $$('.container_destination');
+
+desNavItems.forEach((item, index) => {
+    item.onclick = function () {
+        const containerDestination = containerDestinations[index];
+
+        $('.container_destination.active').classList.remove('active');
+        $('.destination_nav-item.active').classList.remove('active');
+
+        containerDestination.classList.add('active');
+        this.classList.add('active');
+    }
 });
